3230bf8af4ab804d3a5b54b376c8b1f1a83c20d5
Rafael Tosto <rafatosto40@gmail.com>
2024-11-29 20:07:00 -0300

    É isso aí mesmo

--- src/components/Pesquisa.vue
+++ src/components/Pesquisa.vue
@@ -9,7 +9,7 @@ console.log(searchData.value);
 <template>
     <div v-for="search in searchData" :key="search.searchId">
         <div class="transition-all hover:scale-105 transform-gpu bg-indigoButtomColor p-6 rounded-2xl shadow-md">
-            <a href="{{search.searchId}}.html">
+            <a :href="`${search.searchId}.html`">
                 <h2 class="text-2xl text-red-600 font-bold">{{ search.title }}</h2>
                 <p class="text-white mt-2 mb-4">
                     {{ search.summary }}
