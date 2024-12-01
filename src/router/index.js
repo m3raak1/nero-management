344e6724b5c81420a7f42dde16d5862610da8c74
Rafael Tosto <rafatosto40@gmail.com>
2024-11-30 22:16:18 -0300

    Rota home de endereço '/' criada

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import PesquisasView from '../views/PesquisasView.vue';
+import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
             path: '/pesquisas.html',
             name: 'pesquisas',
             component: PesquisasView
+        },
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
         }
     ]
 });
