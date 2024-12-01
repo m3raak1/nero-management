b8aea0504dc68124372a0a8c76e67364334297b8
Rafael Tosto <rafatosto40@gmail.com>
2024-12-01 13:48:11 -0300

    Configurando rotas dinâmicas

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import PesquisasView from '../views/PesquisasView.vue';
 import HomeView from '../views/HomeView.vue';
+import AbrirPesquisaView from '../views/AbrirPesquisaView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
         {
             path: '/pesquisas.html',
             name: 'pesquisas',
             component: PesquisasView
         },
         {
-            path: '/',
-            name: 'home',
-            component: HomeView
+            path: '/pesquisas/:id.html',
+            name: 'abrirpesquisa',
+            component: AbrirPesquisaView
         }
     ]
 });
