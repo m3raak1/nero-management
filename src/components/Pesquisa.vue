e0656772c2eb916be0491b8fdfc3af52288899ea
Anthony Passos <anthony.passos@sempreceub.com>
2024-12-01 11:35:56 -0300

    cores gradientes adicionadas

--- src/components/Pesquisa.vue
+++ src/components/Pesquisa.vue
@@ -13,7 +13,7 @@ defineProps({
     <div v-for="search in filteredResults" :key="search.searchId">
         <div class="transition-all hover:scale-105 transform-gpu bg-indigoButtomColor p-6 rounded-2xl shadow-md border-borderColor border">
             <a :href="`pesquisa${search.searchId}.html`">
-                <h2 class="text-2xl text-red-600 font-bold">{{ search.title }}</h2>
+                <h2 class="text-2xl text-transparent bg-red-gradient bg-clip-text font-bold">{{ search.title }}</h2>
                 <p class="text-white mt-2 mb-4">
                     {{ search.summary }}
                 </p>
