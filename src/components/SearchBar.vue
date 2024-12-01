25195e1e5f77db4fdf5ad4557f5ae7d2dc5dcbbe
Anthony Passos <anthony.passos@sempreceub.com>
2024-12-01 16:33:55 -0300

    Adicionando filtros

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,5 +1,6 @@
 <script setup>
 import searchIcon from '../assets/img/procurar 1.png'
+import Svg from './Svg.vue';
 
 const emit = defineEmits(['search'])
 const search = (e) => {
@@ -10,11 +11,19 @@ const search = (e) => {
 </script>
 
 <template>
-    <form class="flex items-center w-full rounded-full bg-indigoBackground border border-borderColor">
+    <form class="flex items-center w-full rounded-full bg-indigoBackground border border-borderColor relative">
         <label class="sr-only">Buscar pesquisas</label>
         <img :src="searchIcon" class=" z-10 pointer-events-none pl-4"/>
         <div class="pl-3 w-full">
             <input type="text" @input="search" class="py-3 pl-1 w-full outline-none rounded-full bg-indigoBackground text-gray-200 text-xl font-sans" placeholder="Buscar">
+        </div>
+        <div class="absolute -translate-x-1/2 left-1/2  top-full z-10 flex items-center flex-col w-5/6 h-full">
+            <div class="w-full bg-indigoBackground border-borderColor border-2 p-4 rounded-b-2xl">
+                <h2 class="text-transparent bg-clip-text bg-silver-gradient text-xl font-semibold">Filtros</h2>
+                <label for="">Equipe</label>
+                <input type="search">
+            </div>
+            <Svg name="down-pointer" class="justify-self-auto my-3 absolute top-full"/>
         </div>
     </form>
 </template>
