dd5651f24bafeca63695c29ddd6e39dc100373c2
Rafael Tosto <rafatosto40@gmail.com>
2024-12-01 13:52:08 -0300

    Resolvendo merge conflict

--- src/components/Pesquisa.vue
+++ src/components/Pesquisa.vue
@@ -12,7 +12,7 @@ defineProps({
 <template>
     <div v-for="search in filteredResults" :key="search.researchId">
         <div class="transition-all hover:scale-105 transform-gpu bg-indigoButtomColor p-6 rounded-2xl shadow-md border-borderColor border">
-            <a :href="`pesquisa/${search.researchId}.html`">
+            <a :href="`pesquisas/${search.researchId}.html`">
                 <h2 class="text-2xl text-red-600 font-bold">{{ search.title }}</h2>
                 <p class="text-white mt-2 mb-4">
                     {{ search.summary }}
