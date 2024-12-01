4b555b274ebd3f0d42217e140aad5c145f37f197
Rafael Tosto <rafatosto40@gmail.com>
2024-11-30 21:02:10 -0300

    O FILTRO DE BUSCA TA FUNCIONANDO!!!!!!!!!!!!!!

--- src/components/Pesquisas.vue
+++ src/components/Pesquisas.vue
@@ -1,6 +1,6 @@
 <script setup>
 import Pesquisa from '@/components/Pesquisa.vue';
-import SearchInput from 'vue-search-input';
+import SearchBar from '@/components/SearchBar.vue';
 </script>
 
 <template>
@@ -8,10 +8,11 @@ import SearchInput from 'vue-search-input';
         <section class="px-12 pt-12 text-white text-4xl font-sans">
             <span>Pesquisas</span>
         </section>
-        <!-- Developers and Employers -->
+        <!-- Aba de pesquisas -->
         <section class="py-8 px-12">
             <div class=" border rounded-3xl bg-indigoNavbarBg border-indigoNavbarSt">
                 <div class="grid grid-cols-1 md:grid-cols-1 gap-4 p-12 rounded-lg">
+                    <SearchBar />
                 </div>
                 <div class="grid grid-cols-1 md:grid-cols-1 gap-4 p-12 rounded-lg">
                     <Pesquisa />
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,19 @@
+<script setup>
+import searchIcon from '../assets/img/procurar 1.png'
+const emit = defineEmits([search])
+const search = (e) => {
+    console.log(e.target.value);
+    emit('search', e.target.value)
+};
+
+</script>
+
+<template>
+    <form class="flex items-center w-full rounded-full bg-indigoBackground">
+        <label class="sr-only">Buscar pesquisas</label>
+        <img :src="searchIcon" class=" z-10 pointer-events-none pl-4"/>
+        <div class="pl-3 w-full">
+            <input type="text" @input="search" class=" h-10 w-full outline-none rounded-full bg-indigoBackground text-gray-200 text-xl font-sans" placeholder="Buscar">
+        </div>
+    </form>
+</template>
