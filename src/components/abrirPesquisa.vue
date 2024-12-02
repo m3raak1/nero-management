ca1a7c3558b4ea5771626ae2c8eb77cd84c0f21b
Rafael Tosto <rafatosto40@gmail.com>
2024-12-01 21:25:09 -0300

    Finalmente funciona!!!!!!!! Boraaaaa

--- src/components/abrirPesquisa.vue
+++ src/components/abrirPesquisa.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Svg from './Svg.vue';
 import { useRoute } from 'vue-router';
 import { onMounted, ref, computed } from 'vue';
 //Obtém o id desejado
@@ -21,24 +22,41 @@ const researchById = computed(() => {
 
 <template>
     <div class="flex-1 h-screen overflow-y-scroll scroll-m-1">
-        <section class="px-12 pt-12 text-white text-4xl font-sans">
-            <span>Pesquisas: {{ $route.params.id }}</span>
+        <section class="px-12 pt-12 text-4xl font-sans">
+            <span class="text-transparent bg-silver-gradient bg-clip-text">Pesquisas</span>
         </section>
         <section class="py-8 px-12">
-            <div class=" min-h-screen border rounded-3xl bg-indigoNavbarBg border-indigoNavbarSt">
-                <div class=" p-7 min-h-64 border-b-2 border-indigoNavbarSt">
-                    <div v-if="researchById.length > 0" class="flex justify-between">
+            <div v-if="researchById.length > 0"
+                class=" min-h-screen border rounded-3xl bg-indigoNavbarBg border-indigoNavbarSt">
+                <div class=" p-7  border-b-2 border-indigoNavbarSt">
+                    <div class="flex justify-between">
                         <h2 class="text-2xl text-transparent bg-red-gradient bg-clip-text font-bold">{{
                             researchById[0].title }}</h2>
                         <div class="h-auto flex">
                             <span
-                                class="text-white h-full flex items-center bg-sapphire-gradient px-4 rounded-3xl text-sm">Responsável:
+                                class="text-white flex items-center bg-metal-gradient py-2 px-4 rounded-3xl text-sm">Equipe:
+                                Alfa</span>
+                            <span
+                                class="text-white h-full flex items-center bg-sapphire-gradient px-4 ml-4 rounded-3xl text-sm">Responsável:
                                 {{ researchById[0].responsible }}</span>
                             <span
                                 class="text-white h-full flex items-center bg-emerald-gradient px-4 ml-4 rounded-3xl text-sm">Status:
                                 {{ String(researchById[0].status).charAt(0).toUpperCase() +
                                     String(researchById[0].status).slice(1)
                                 }}</span>
+                        </div>
+                    </div>
+                    <p class="pt-10 text-white">
+                        {{ researchById[0].objective }}
+                    </p>
+                    <div class="pt-10 flex justify-between text-white">
+                        <div class="flex items-center gap-2">
+                            <p>Data de inicio </p>
+                            <span
+                                class="text-white flex items-center justify-between bg-ruby-gradient py-2 px-4 rounded-3xl text-sm">
+                                <p class="my-auto">{{ researchById[0].startDate }}</p> <Svg name="date"
+                                    class="ml-2 -mt-1" />
+                            </span>
                         </div>
                     </div>
                 </div>
