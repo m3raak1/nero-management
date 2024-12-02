f526b0d56f44a510244dae2b60d4b920d8ca7f67
Anthony Passos <anthony.passos@sempreceub.com>
2024-12-02 18:01:52 -0300

    adicionando verificação de input visual

--- src/components/Pesquisas/NovaPesquisa.vue
+++ src/components/Pesquisas/NovaPesquisa.vue
@@ -19,7 +19,7 @@ onMounted(async () => {
 const inputData = ref({
     title: '',
     team: '',
-    startDate: Date,
+    startDate: '',
     responsible: '',
     objective: '',
     summaryTitle: '',
@@ -42,35 +42,35 @@ const submitResearch = (e) => {
                 <div class=" p-7">
                     <div class="w-full flex justify-between items-center gap-10">
                         <!-- Titulo da Pesquisa-->
-                        <input v-model="inputData.title" class="text-xl py-2 px-2 border-b border-borderColor w-2/5" type="text" placeholder="Inserir Titulo">
+                        <input v-model="inputData.title" :class="['text-xl py-2 px-2 border-b ', inputData.title !== '' ? 'border-borderColor' : 'border-red-600']" type="text" placeholder="Inserir Titulo">
                         <div class="flex gap-10">
                             <!-- Equipe -->
                             <div class="flex items-center gap-2">
-                                <label>Equipe</label>
+                                <label :class="['border-b', inputData.team !== '' ? 'border-transparent' : 'border-red-600']">Equipe</label>
                                 <select v-model="inputData.team" class="px-4 py-1 bg-metal-gradient rounded-3xl">
                                     <option class="bg-indigoNavbarBg" v-for="(team, i) in teamsCatalog" :key="i" :value="team.name">{{ team.name }}</option>
                                 </select>
                             </div>
                             <!-- Data -->
                             <div class="flex items-center gap-2">
-                                <label>Data de Inicio</label>
+                                <label :class="['border-b', inputData.startDate !== '' ? 'border-transparent' : 'border-red-600']">Data de Inicio</label>
                                 <input v-model="inputData.startDate" class="bg-ruby-gradient rounded-2xl px-4 py-1" type="date">
                             </div>
                         </div>
                     </div>
                     <!-- Responsavel-->
                     <div class="my-6">
-                        <input v-model="inputData.responsible" class="text-lg py-2 px-2 border-b border-borderColor" list="users" placeholder="Responsavel">
+                        <input v-model="inputData.responsible" :class="['text-lg py-2 px-2 border-b border-borderColor', inputData.responsible !== '' ? 'border-borderColor' : 'border-red-600']" list="users" placeholder="Responsavel">
                         <datalist id="users" placeholder="Responsavel">
                             <option v-for="(user, i) in usersCatalog" :key="i" :value="user.name"></option>
                         </datalist>
                     </div>
                     <!-- Objetivo -->
-                    <textarea v-model="inputData.objective" class="w-full bg-indigoBackground rounded-2xl p-4" rows="5" placeholder="Objetivo da Pesquisa"></textarea>
+                    <textarea v-model="inputData.objective" :class="['w-full bg-indigoBackground rounded-2xl p-4 border', inputData.objective !== '' ? 'border-transparent' : 'border-red-600']" rows="5" placeholder="Objetivo da Pesquisa"></textarea>
                     <!-- Titulo Resumo -->
-                    <input v-model="inputData.summaryTitle" class="text-xl py-2 px-2 my-6 w-2/5 border-b border-borderColor" type="text" placeholder="Inserir Titulo do Resumo">
+                    <input v-model="inputData.summaryTitle" :class="['text-xl py-2 px-2 my-6 w-2/5 border-b border-borderColor', inputData.summaryTitle !== '' ? 'border-borderColor' : 'border-red-600']" type="text" placeholder="Inserir Titulo do Resumo">
                     <!-- Resumo -->
-                    <textarea v-model="inputData.summary" class="w-full bg-indigoBackground rounded-2xl p-4" rows="5" placeholder="Resumo da Pesquisa"></textarea>
+                    <textarea v-model="inputData.summary" :class="['w-full bg-indigoBackground rounded-2xl p-4 border', inputData.summary !== '' ? 'border-transparent' : 'border-red-600']" rows="5" placeholder="Resumo da Pesquisa"></textarea>
                 </div>
             </div>
             <button @click="submitResearch" class=" bg-indigoNavbarBg border border-borderColor rounded-2xl py-2 px-4 font-semibold my-4">
@@ -83,5 +83,9 @@ const submitResearch = (e) => {
 <style scoped>
     input, select{
         @apply bg-transparent outline-none
+    }
+
+    textarea {
+        outline: none;
     }
 </style>
