ee4db96e671a41eb4a3231d897ffe3d931f79923
Anthony Passos <anthony.passos8@gmail.com>
2024-12-10 09:54:40 -0300

    mudando a rota /

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ const router = createRouter({
         {
             path: '/',
             name: 'home',
-            component: HomeView
+            component: PesquisasView
         },
         {
             path: '/login',
